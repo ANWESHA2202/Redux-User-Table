0d258d556a1e606eecfc79ea78f2043647a4c65e
Anwesha SAnyal <anwesha.sanyal22@gmail.com>
2023-07-24 03:15:42 +0530

    delete all users

--- src/components/DeleteAllUsers.jsx
+++ src/components/DeleteAllUsers.jsx
@@ -1,10 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import {useDispatch} from 'react-redux'
+import styled from 'styled-components'
+import {deleteUsers} from '../store/slices/UserSlice'
 
 const DeleteAllUsers = () => {
+  const dispatch=useDispatch();
+  const deleteAllUsers=()=>{
+    dispatch(deleteUsers())
+  }
   return (
-    <div>DeleteAllUsers</div>
+    <Wrapper>
+      <button className='btn clear-btn' onClick={()=>deleteAllUsers()}>Delete All Users</button>
+    </Wrapper>
   )
 }
+
+const Wrapper=styled.section`
+.btn{
+  background-color: red;
+  margin-top:2rem;
+}
+`
 
 export default DeleteAllUsers
