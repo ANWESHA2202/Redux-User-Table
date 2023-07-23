7e857af94e5184239377bbcbfe86584609cc2613
Anwesha SAnyal <anwesha.sanyal22@gmail.com>
2023-07-23 17:11:54 +0530

    basic ui using styled-components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './components/NavBar'
+import UserDetails from './components/UserDetails'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <UserDetails/>
+    </>
+  )
+}
+
+export default App
